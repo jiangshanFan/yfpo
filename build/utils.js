c3ba3604e9a86a5c59b9913b57b486eb3a9223c9
jiangshan.fan <745989449@qq.com>
2019-06-24 15:37:33 +0800

    Main{ Fixed[utils.js, add publicPath: '../../' in Extract CSS(vue-style-loader)] }

--- build/utils.js
+++ build/utils.js
@@ -47,7 +47,8 @@ exports.cssLoaders = function (options) {
     if (options.extract) {
       return ExtractTextPlugin.extract({
         use: loaders,
-        fallback: 'vue-style-loader'
+        fallback: 'vue-style-loader',
+        publicPath: '../../'              //-----从dist目录打开index.html文件需要加上此路径
       })
     } else {
       return ['vue-style-loader'].concat(loaders)
